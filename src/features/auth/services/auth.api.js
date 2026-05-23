7511f1b6bffc5f3377c06b2d12a5cc84a7d938fd
Jashandeep Singh <jashannx@gmail.com>
2026-05-24 00:02:40 +0530

    backend config

--- src/features/auth/services/auth.api.js
+++ src/features/auth/services/auth.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api/auth',
+    baseURL: `${import.meta.env.VITE_API_URI}/api/auth`,
     withCredentials: true
 })
 export async function registerUser(username, email, password) {
--- src/features/interview/services/interview.api.js
+++ src/features/interview/services/interview.api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: `${import.meta.env.VITE_API_URI}`,
     withCredentials: true
 });
 
